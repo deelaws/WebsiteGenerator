4757349d3d0ceea0d3d25ea18bbf1a1f3010f47f
Waleed <deelaws89@gmail.com>
2016-09-21 23:24:26 -0700

    Basic template rendering program.

--- WebsiteGenerator/WebsiteGenerator.py
+++ WebsiteGenerator/WebsiteGenerator.py
@@ -1,1 +1,11 @@
-from jinja2 import compiler
+from jinja2 import Environment, PackageLoader
+
+
+
+env = Environment(loader=PackageLoader('WebsiteGenerator', 'html'))
+
+template = env.get_template('index_template.html')
+
+print(template.render(title="Main Page"))
+
+print("==========================================")
