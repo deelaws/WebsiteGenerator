b04406ec2ba635790927d08f2c03b4b6263dc8a0
Waleed <deelaws89@gmail.com>
2016-09-22 22:25:18 -0700

    Add basic header and footer.

--- WebsiteGenerator/WebsiteGenerator.py
+++ WebsiteGenerator/WebsiteGenerator.py
@@ -1,11 +1,29 @@
 from jinja2 import Environment, PackageLoader
+import sys
 
 
 
-env = Environment(loader=PackageLoader('WebsiteGenerator', 'html'))
+def generate_index_page(env):
+    template = env.get_template('base_template.html')
+    a = template.render(title='Waleed\'s website')
+    #a = a.encode(sys.stdout.encoding, errors='replace')
+    #print(type(a))
+    index_file = open('index.html', mode='w', encoding='utf8')
+    index_file.writelines(a)
+    index_file.flush()
+    index_file.close()
 
-template = env.get_template('index_template.html')
 
-print(template.render(title="Main Page"))
+if __name__ == "__main__":
+    
+    print(sys.stdout.encoding)
+    pack_load = PackageLoader('WebsiteGenerator', 'html')
+    templates = pack_load.list_templates()
+        
+    # List all the available templates
+    for t in templates:
+        print(t)
 
-print("==========================================")
+    env = Environment(loader=pack_load)
+
+    generate_index_page(env)
